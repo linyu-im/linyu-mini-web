6fe3c49cbe9b339dec5b43370e146a8555132063
Heath <duweihengmr@163.com>
2024-12-18 23:20:58 +0800

    红点提示

---
+++ src/components/LinyuDotHint.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dot-hint">
+    {{ props.text }}
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  text: String
+})
+</script>
+<style lang="less" scoped>
+.dot-hint {
+  padding: 0 4px;
+  min-width: 18px;
+  height: 18px;
+  background: #ff4c4c;
+  color: #FFFFFF;
+  font-size: 10px;
+  line-height: 1;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 5px;
+  font-weight: 600;
+  user-select: none;
+}
+</style>
